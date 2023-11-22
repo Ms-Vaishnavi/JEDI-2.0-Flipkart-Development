5997becd36bc56cfef890f5d6f884314002dc3fc
Vaishnavi Sahu <vaishnavisahu16@gmail.com>
2023-11-22 23:33:31 +0530

    Adds Business classes

---
+++ GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/business/GymOwnerBusiness.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.flipkart.business;
+
+import com.flipkart.bean.Customer;
+import com.flipkart.bean.Gym;
+import java.util.*;
+
+/**
+ * 
+ */
+public class GymOwnerBusiness {
+
+	public void getProfile(Customer customer) {
+
+	}
+
+	public void editProfile(Customer customer) {
+
+	}
+	
+	public boolean addGym(Gym gym) {
+		return false;
+	}
+	
+	public void editGym(Gym gym) {
+		
+	}
+	
+	public List<Gym> getGymDetail() {
+		return new ArrayList<>();
+	}
+	
+	public boolean isApproved(String email) {
+		return false;
+	}
+	
+	public boolean isGymApproved(Gym gym) {
+		return false;
+	}
+}
