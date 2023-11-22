bc285f404e2b4f5d3f567268f38ca363cc11b3ad
Sai Sudha <saisudha2408@gmail.com>
2023-11-22 22:50:39 +0530

    Add Beans

---
+++ GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/bean/Administrator.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * 
+ */
+public class Administrator extends User {
+	private String name;
+	private String phoneNumber;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/bean/GymOwner.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * 
+ */
+public class GymOwner extends User {
+	private String name;
+	private String phoneNumber;
+	private String aadharNumber;
+	private String panNumber;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAadharNumber() {
+		return aadharNumber;
+	}
+
+	public void setAadharNumber(String aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+
+	public String getPanNumber() {
+		return panNumber;
+	}
+
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+
+}
