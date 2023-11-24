23f053ef19ff1986d84f04e5010e56e8b6396571
Sarthak Bhatt <sarthak.bhatt314@gmail.com>
2023-11-24 14:54:36 +0530

    Formatting changes to CustomerClient.java and GymOwner.java

--- GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/client/GymOwnerClient.java
+++ GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/client/GymOwnerClient.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.flipkart.client;
 
 import java.util.List;
@@ -10,151 +7,144 @@ import com.flipkart.bean.Gym;
 import com.flipkart.bean.GymOwner;
 import com.flipkart.business.GymOwnerBusiness;
 import com.flipkart.business.UserBusiness;
-import com.flipkart.constants.*;
+import com.flipkart.constants.ColorConstants;
 
-/**
- * 
- */
 public class GymOwnerClient {
 
-	GymOwner gymOwner = new GymOwner();
-	GymOwnerBusiness gymOwnerBusiness = new GymOwnerBusiness();
+    GymOwner gymOwner = new GymOwner();
+    GymOwnerBusiness gymOwnerBusiness = new GymOwnerBusiness();
 
-	/**
-	 * @param args
-	 */
+    public void gymOwnerRegistration(Scanner in) {
+        System.out.println("Enter GymOwner Details: ");
+        System.out.print("Enter Name: ");
+        gymOwner.setName(in.next());
+        System.out.print("Enter Phone Number: ");
+        gymOwner.setPhoneNumber(in.next());
+        System.out.print("Enter PAN: ");
+        gymOwner.setPanNumber(in.next());
+        System.out.print("Enter Aadhaar: ");
+        gymOwner.setAadharNumber(in.next());
+        System.out.print("Enter Email: ");
+        gymOwner.setEmail(in.next());
+        System.out.print("Enter Password: ");
+        gymOwner.setPassword(in.next());
+        gymOwner.setRoleId("GymOwner");
 
-	public void gymOwnerRegistration(Scanner in) {
-		System.out.println("Enter GymOwner Details : ");
-		System.out.println("Enter Name : ");
-		gymOwner.setName(in.next());
-		System.out.println("Enter Phone Number : ");
-		gymOwner.setPhoneNumber(in.next());
-		System.out.println("Enter PAN : ");
-		gymOwner.setPanNumber(in.next());
-		System.out.println("Enter Aadhaar : ");
-		gymOwner.setAadharNumber(in.next());
-		System.out.println("Enter Email : ");
-		gymOwner.setEmail(in.next());
-		System.out.println("Enter Password : ");
-		gymOwner.setPassword(in.next());
-		gymOwner.setRoleId("GymOwner");
+        UserBusiness userBusiness = new UserBusiness();
+        userBusiness.registerGymOwner(gymOwner);
 
-		UserBusiness userBusiness = new UserBusiness();
-		userBusiness.registerGymOwner(gymOwner);
+        System.out.println("\n" + ColorConstants.CYAN + "Customer registered successfully!" + ColorConstants.RESET);
+    }
 
-		System.out.println("\n" + ColorConstants.CYAN + "Customer registered successfully!" + ColorConstants.RESET);
-	}
+    public void editProfile(Scanner in, String email) {
+        System.out.println("Enter Details: ");
+        System.out.print("Enter Name: ");
+        gymOwner.setName(in.next());
+        System.out.print("Enter Phone Number: ");
+        gymOwner.setPhoneNumber(in.next());
+        System.out.print("Enter PAN: ");
+        gymOwner.setPanNumber(in.next());
+        System.out.print("Enter Aadhaar: ");
+        gymOwner.setAadharNumber(in.next());
 
-	public void editProfile(Scanner in, String email) {
-		System.out.println("Enter Details: ");
-		System.out.println("Enter Name : ");
-		gymOwner.setName(in.next());
-		System.out.println("Enter Phone Number : ");
-		gymOwner.setPhoneNumber(in.next());
-		System.out.println("Enter PAN : ");
-		gymOwner.setPanNumber(in.next());
-		System.out.println("Enter Aadhaar : ");
-		gymOwner.setAadharNumber(in.next());
+        gymOwnerBusiness.editProfile(gymOwner, email);
+    }
 
-		gymOwnerBusiness.editProfile(gymOwner, email);
-	}
+    public void viewProfile(Scanner in, String email) {
+        gymOwner = gymOwnerBusiness.getProfile(email);
+        System.out.println("______________________________________________________________");
+        System.out.printf("%15s%15s%15s%15s", "Gym Owner Name", "Phone Number", "PAN Number", "Aadhaar Number");
+        System.out.println();
+        System.out.printf("%15s%15s%15s%15s", gymOwner.getName(), gymOwner.getPhoneNumber(),
+                gymOwner.getPanNumber(), gymOwner.getAadharNumber());
+        System.out.println("______________________________________________________________");
 
-	public void viewProfile(Scanner in, String email) {
-		gymOwner = gymOwnerBusiness.getProfile(email);
-		System.out.println("______________________________________________________________");
-		System.out.printf("%15s%15s%15s%15s", "Gym Owner Name", "Phone Number", "PAN Number", "Aadhaar Number");
-		System.out.println();
-		System.out.printf("%15s%15s%15s%15s", gymOwner.getName(), gymOwner.getPhoneNumber(), gymOwner.getPanNumber(),
-				gymOwner.getAadharNumber());
-		System.out.println("______________________________________________________________");
+    }
 
-	}
+    public void addGym(Scanner in, String email) {
+        System.out.println("Enter Gym Details: ");
 
-	public void addGym(Scanner in, String email) {
-		System.out.println("Enter Gym Details: ");
+        Gym gym = new Gym();
+        System.out.print("Enter GymName: ");
+        gym.setGymName(in.next());
+        gym.setOwnerEmail(email);
+        System.out.print("Enter Address: ");
+        gym.setAddress(in.next());
+        System.out.print("Enter SlotCount: ");
+        gym.setSlotCount(in.nextInt());
+        System.out.print("Enter seatsPerSlotCount: ");
+        gym.setSeatsPerSlotCount(in.nextInt());
+        gym.setVerified(false);
 
-		Gym gym = new Gym();
-		System.out.println("Enter GymName : ");
-		gym.setGymName(in.next());
-		gym.setOwnerEmail(email);
-		System.out.println("Enter Address : ");
-		gym.setAddress(in.next());
-		System.out.println("Enter SlotCount : ");
-		gym.setSlotCount(in.nextInt());
-		System.out.println("Enter seatsPerSlotCount : ");
-		gym.setSeatsPerSlotCount(in.nextInt());
-		gym.setVerified(false);
+        gymOwnerBusiness.addGym(gym);
 
-		gymOwnerBusiness.addGym(gym);
+    }
 
-	}
+    public void editGym(Scanner in, String email) {
+        System.out.println("Enter Gym Details: ");
 
-	public void editGym(Scanner in, String email) {
-		System.out.println("Enter Gym Details: ");
+        Gym gym = new Gym();
+        System.out.print("Enter GymId: ");
+        gym.setGymId(in.next());
+        System.out.print("Enter GymName: ");
+        gym.setGymName(in.next());
+        gym.setOwnerEmail(email);
+        System.out.print("Enter Address: ");
+        gym.setAddress(in.next());
+        System.out.print("Enter SlotCount: ");
+        gym.setSlotCount(in.nextInt());
+        System.out.print("Enter seatsPerSlotCount: ");
+        gym.setSeatsPerSlotCount(in.nextInt());
+        gym.setVerified(false);
 
-		Gym gym = new Gym();
-		System.out.println("Enter GymId : ");
-		gym.setGymId(in.next());
-		System.out.println("Enter GymName : ");
-		gym.setGymName(in.next());
-		gym.setOwnerEmail(email);
-		System.out.println("Enter Address : ");
-		gym.setAddress(in.next());
-		System.out.println("Enter SlotCount : ");
-		gym.setSlotCount(in.nextInt());
-		System.out.println("Enter seatsPerSlotCount : ");
-		gym.setSeatsPerSlotCount(in.nextInt());
-		gym.setVerified(false);
+        gymOwnerBusiness.editGym(gym);
+    }
 
-		gymOwnerBusiness.editGym(gym);
-	}
+    public void getGymDetails(Scanner in, String email) {
+        List<Gym> gymDetails = gymOwnerBusiness.getGymDetail(email);
+        for (Gym gym : gymDetails) {
+            System.out.println(gym);
+        }
+    }
 
-	public void getGymDetails(Scanner in, String email) {
-		List<Gym> gymDetails = gymOwnerBusiness.getGymDetail(email);
-		for (Gym gym : gymDetails) {
-			System.out.println(gym);
-		}
-	}
+    public void gymOwnerMenu(Scanner in, String email) {
+        boolean recur = true;
+        while (recur) {
 
-	public void gymOwnerMenu(Scanner in, String email) {
-		boolean recur = true;
-		while (recur) {
+            System.out.println("\nHere are the actions you can perform!");
 
-			System.out.println("\nHere are the actions you can perform!");
+            System.out.println("1. View Profile");
+            System.out.println("2. Edit Profile");
+            System.out.println("3. Add Gym");
+            System.out.println("4. Edit Gym");
+            System.out.println("5. View All Gym Details");
+            System.out.println("6. Exit\n");
 
-			System.out.println("1. View Profile");
-			System.out.println("2. Edit Profile");
-			System.out.println("3. Add Gym");
-			System.out.println("4. Edit Gym");
-			System.out.println("5. View All Gym Details");
-			System.out.println("6. Exit\n");
+            int choice = in.nextInt();
 
-			int choice = in.nextInt();
+            switch (choice) {
+                case 1:
+                    viewProfile(in, email);
+                    break;
+                case 2:
+                    editProfile(in, email);
+                    break;
+                case 3:
+                    addGym(in, email);
+                    break;
+                case 4:
+                    editGym(in, email);
+                    break;
+                case 5:
+                    getGymDetails(in, email);
+                    break;
 
-			switch (choice) {
-			case 1:
-				viewProfile(in, email);
-				break;
-			case 2:
-				editProfile(in, email);
-				break;
-			case 3:
-				addGym(in, email);
-				break;
-			case 4:
-				editGym(in, email);
-				break;
-			case 5:
-				getGymDetails(in, email);
-				break;
+                case 6:
+                    recur = false;
+                    break;
+            }
+        }
 
-			case 6:
-				recur = false;
-				break;
-			}
-		}
-
-	}
+    }
 
 }
