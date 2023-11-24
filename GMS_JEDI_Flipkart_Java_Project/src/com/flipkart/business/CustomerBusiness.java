02ea31104cc6316c802d725536bf247974c29b7f
anjalip131 <prajapatianjali.d@BLRETV-LM7VXPKX.local>
2023-11-24 11:34:19 +0530

    adding hardcoded customer values

--- GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/business/CustomerBusiness.java
+++ GMS_JEDI_Flipkart_Java_Project/src/com/flipkart/business/CustomerBusiness.java
@@ -8,6 +8,20 @@ import java.util.*;
 
 public class CustomerBusiness {
 
+	List<Customer> customers = new ArrayList<>();
+	Customer customer1 = new Customer("c1@gmail.com", "c1", "Customer", "Vaishnavi", "0000", 22, "Kanpur");
+	Customer customer2 = new Customer("c2@gmail.com", "c2", "Customer", "Anjali", "0000", 32, "Vadodara");
+	Customer customer3 = new Customer("c3@gmail.com", "c3", "Customer", "Sudha", "0000", 42, "Kolkata");
+	Customer customer4 = new Customer("c4@gmail.com", "c4", "Customer", "Aaishu", "0000", 52, "Mumbai");
+
+	public CustomerBusiness()
+	{
+		customers.add(customer1);
+		customers.add(customer2);
+		customers.add(customer3);
+		customers.add(customer4);
+	}
+	
 	public void getProfile(Customer customer) {
 
 	}
