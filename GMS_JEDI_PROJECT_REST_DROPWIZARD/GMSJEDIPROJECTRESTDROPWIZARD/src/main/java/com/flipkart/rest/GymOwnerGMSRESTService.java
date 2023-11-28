30b5b6bbc33dd61c16ed3cdb01bc9ff6e55bd1ba
Sai Sudha <saisudha2408@gmail.com>
2023-11-28 21:11:36 +0530

    Update main class in pom.xml

--- GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIPROJECTRESTDROPWIZARD/src/main/java/com/flipkart/rest/GymOwnerGMSRESTService.java
+++ GMS_JEDI_PROJECT_REST_DROPWIZARD/GMSJEDIPROJECTRESTDROPWIZARD/src/main/java/com/flipkart/rest/GymOwnerGMSRESTService.java
@@ -1,11 +1,37 @@
-/**
- * 
- */
 package com.flipkart.rest;
 
-/**
- * API Calls to Gym Owner service
- */
-public class GymOwnerGMSRESTService {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.codahale.metrics.annotation.Timed;
+import com.flipkart.bean.GymOwner;
+import com.flipkart.business.GymOwnerBusiness;
+import com.flipkart.business.GymOwnerBusinessInterface;
+import com.flipkart.business.UserBusiness;
+import com.flipkart.exception.UserAlreadyExistsException;
+
+@Path("/v1/gymowners")
+@Produces(MediaType.APPLICATION_JSON)
+public class GymOwnerGMSRESTService {
+	
+	@POST
+    @Path("/registration")
+    @Timed
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response registerGymOwner(GymOwner gymOwner) {
+        GymOwnerBusinessInterface gymOwnerBusiness = new GymOwnerBusiness();
+        UserBusiness userBusiness = new UserBusiness();
+
+        try {
+            userBusiness.registerGymOwner(gymOwner);
+            return Response.ok("Gym Owner registered successfully!").build();
+        } catch (UserAlreadyExistsException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
 }
